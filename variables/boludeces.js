a658f9eba5cf670b75e046c8812eceb0d669a496
Leandro Battocchio <leandrobattocchio@hotmail.com>
2022-10-16 23:14:06 -0300

    fix

--- variables/boludeces.js
+++ variables/boludeces.js
@@ -3,11 +3,9 @@ let juank = 0
 function boludo () {
   juank++
   if (juank <= 15) {
-    console.log(juank)
     return false
   } else {
     juank = 0
-    console.log(`${juank} paso`)
     return true
   }
 }
