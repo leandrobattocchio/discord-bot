6f298c8ae427498ac1d0eeabf92b3341dda780b9
Leandro Battocchio <leandrobattocchio@hotmail.com>
2022-10-16 23:27:44 -0300

    changing answers

--- commands/crystall-ball.js
+++ commands/crystall-ball.js
@@ -1,21 +1,9 @@
 const { SlashCommandBuilder } = require('discord.js')
 const respuestas = {
-  0: 'Si papa.',
-  1: 'Probablemente genio.',
-  2: 'Si sos de pigmentacion negra podria ser complicado.',
-  3: 'Tal vez...',
-  4: 'Ni en pedo.',
-  5: 'Si, dale, segui queriendo.',
-  6: 'Depende de si Messi quiere.',
-  7: 'Quizas, si depende del ogro cagaste.',
-  8: 'Mientras el juank siga errando goles seguramente, asi que si.',
-  9: 'Me mataste pa, no soy de aca.',
-  10: 'Preguntale al diego, que me rompes las bolas a mi.',
-  11: 'No, al igual que Central ganando un campeonato cagaste',
-  12: 'Esta mas al horno que patronato',
-  13: 'Jajajajaja',
-  14: 'Tu mama',
-  15: 'La mama del juank'
+  0: 'Si',
+  1: 'No',
+  2: 'Tal vez',
+  3: 'Me matas pa, no soy de aca'
 }
 
 module.exports = {
